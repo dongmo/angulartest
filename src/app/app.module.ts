2bc5d2be388ad760decc0e686ab86fde935d5b9f
samueldongmo <dongmo_samuel@yahoo.fr>
2018-08-16 16:40:20 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule,MatGridListModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
 import "hammerjs";
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import {DishService} from './services/dish.service';
+
+import {AppRoutingModule} from '../app/app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -27,9 +43,11 @@ import { MenuComponent } from './menu/menu.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
-    FlexLayoutModule
+    MatGridListModule,
+    FlexLayoutModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
